9ebe43c474a93059614b47caad91b84fa8923cc8
pafnootiy <ksander-86@mail.ru>
2021-11-18 01:15:20 +0300

    Добавил рабочий скрипт

--- main_format_json.py
+++ main_format_json.py
@@ -1,17 +1,29 @@
 import json
+import argparse
+import os
 
 
-def load_data(file_path=r"data.json"):
+def load_data(file_path="data.json"):
+    print(file_path)
     with open(file_path, "r", encoding='utf-8', newline='') as file_obj:
         text = file_obj.read()
     data = json.loads(text)
     return data
 
 
-if __name__ == '__main__':
+def createParser():
+    parser = argparse.ArgumentParser(description="formating .json files")
+    parser.add_argument("data_file_path", type=str, nargs="?")
 
-    data = load_data()
+    return parser
+
+
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
+    print(namespace)
+    data = load_data(file_path=namespace.data_file_path)
     edit_data = json.dumps(data, sort_keys=True,
                            indent=4, ensure_ascii=False)
-
     print(edit_data)
+
